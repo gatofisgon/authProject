06e5adc1de11a5ff54afa5e0bae84e49e790d41f
Kevin <kevinsaezramirez@gmail.com>
2022-03-12 14:42:54 -0300

    V1.1 Added MongoDB (Mongoose)

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
 
 const app = express();
 
+mongoose.connect("mongodb://localhost:27017/userDB");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
+
+const userSchema = {
+    email: String,
+    password: String
+}
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
     res.render("home");
